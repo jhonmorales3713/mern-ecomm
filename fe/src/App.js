72dba867a3cbf7b5597df448d70b03e6b9b2edb6
Jhon Israel Morales <32384131+jhonmorales3713@users.noreply.github.com>
2022-06-27 22:03:29 +0800

    Dashb

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
 function App() {
   return (
     <div>
       <header>
         <a href="/">Toktokmall</a>
       </header>
-      <main>list prods</main>
+      <main>
+        <h1>Featured Products </h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.tag}>
+              <a href={`/product/${product.tag}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.tag}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+                <button>Buy Now</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
