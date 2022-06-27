58a70cc724aa70af78355aa2eaa4a775e7295cb1
Jhon Israel Morales <32384131+jhonmorales3713@users.noreply.github.com>
2022-06-27 21:18:38 +0800

    first commit

---
+++ fe/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Toktokmall</a>
+      </header>
+      <main>list prods</main>
+    </div>
+  );
+}
+
+export default App;
