a92d6d9d215bf0dcc8b5664554685b6411df1074
Jhon Israel Morales <32384131+jhonmorales3713@users.noreply.github.com>
2022-07-15 11:06:05 +0800

    React Context

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,9 +2,13 @@ import data from './data';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
-import { Container, Navbar } from 'react-bootstrap';
+import { Badge, CarouselItem, Container, Nav, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className="d-flex flex-column body-content">
@@ -14,6 +18,16 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>Toktokmall</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
--- fe/src/components/Product.js
+++ fe/src/components/Product.js
@@ -1,8 +1,17 @@
+import { useContext } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Store } from '../Store';
 import Rating from './Rating';
 function Product(props) {
   const { product } = props;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: 'ADD_CART_ITEM',
+      payload: { ...product, quantity: 1 },
+    });
+  };
   return (
     <Card className="product">
       <Link to={`/product/${product.tag}`}>
@@ -14,7 +23,9 @@ function Product(props) {
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>{product.price}</Card.Text>
-        <Button className="btn-add-to-cart">Add To Cart</Button>
+        <Button onClick={addToCartHandler} className="btn-add-to-cart">
+          Add To Cart
+        </Button>
         <Button className="btn-buy-now">Buy Now</Button>
       </Card.Body>
     </Card>
